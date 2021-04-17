6a9f05d3304c370f95b1ca43ee3b505617a88665
ayodejid1 <61972920+ayodejid1@users.noreply.github.com>
2021-04-17 11:58:36 +0100

    update

--- C&OtaskZuri.py
+++ C&OtaskZuri.py
@@ -1,5 +1,7 @@
-# step3
+# steps
+# Create a Budget class that can instantiate objects based on different budget categories like food, clothing, and entertainment.
 #  These objects should allow for:
+#  1.Depositing funds to each of the categories
 #  2.Withdrawing funds from each category
 #  3.Computing category balances
 #  4.Transferring balance amounts between categories
